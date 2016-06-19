d8a2293088dd0c53a9577bedf5d7192834d0d4e4
Philip Ingram <philip.d.ingram@gmail.com>
2016-06-18 18:02:27 -0700

    create collectTests function

--- src/calculator.js
+++ src/calculator.js
@@ -10,7 +10,34 @@ calculator.timeTest = function(n, fn) {
     fn(strToTest);
   }
   const endTime = new Date();
-  return (endTime - startTime) / 1000;
+  return (endTime - startTime);
+}
+
+calculator.collectTests = function(fn) {
+  // these 3 numbers were determined by trial and error for a couple linear functions and could use
+  // optimization
+  const maxTimeForTest = 1000;
+  const minTimeForTest = 10;
+  const increaseFactor = 1.2
+
+  let results = [];
+  let n = 1;
+  let avgTimeTakenLastTest = 0;
+
+  while (avgTimeTakenLastTest < maxTimeForTest) {
+    console.log(n);
+    let totalTimeForTest = 0;
+    for (let i = 0; i < 3; i++) {
+      totalTimeForTest += this.timeTest(n, fn);
+    }
+    avgTimeTakenLastTest = totalTimeForTest / 3;
+    if (avgTimeTakenLastTest > minTimeForTest && avgTimeTakenLastTest < maxTimeForTest) {
+      results.push([n, avgTimeTakenLastTest]);
+    }
+    n *= increaseFactor;
+    console.log(avgTimeTakenLastTest);
+  }
+  return results;
 }
 
 // Based off of Nic Rabboy's function (https://www.thepolyglotdeveloper.com/2015/03/create-a-random-nonce-string-using-javascript/)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const calculator = require('./calculator.js');
 
-console.log(calculator.timeTest(1000000000, testFunction));
+console.log(calculator.collectTests(testFunction));
 
 function testFunction(str) {
   const a = str;
+  // console.log(str);
 }
